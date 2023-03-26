4675d7b35c2e9dc7106d0ae5d0242334c27b2aaf
Aaron Abkemeier <aaronabkemeier@gmail.com>
2023-03-26 16:10:45 -0400

    tidy_pfilter_dfs() now includes shared parameters

--- R/tidy_pfilter_dfs.R
+++ R/tidy_pfilter_dfs.R
@@ -11,28 +11,32 @@
 #' tidy_pfilter_dfs(EL_out)
 tidy_pfilter_dfs = function(x){
   lapply(1:nrow(x$fits), function(z){
-    tidy_LL_df = coef_to_pparams(x$fits[z,])$specific %>%
-      t() %>%
-      as.data.frame() %>%
-      tibble::rownames_to_column(var = "unit") %>%
+    tidy_LL_df = coef_to_pparams(x$fits[z,-c(1,2)])$specific |>
+      t() |>
+      as.data.frame() |>
+      tibble::rownames_to_column(var = "unit") |>
       dplyr::mutate(rep = z)
-    tidy_ull_df = subset(x$ull, subset = rownames(x$ull) == z) %>%
-      t() %>%
-      as.data.frame() %>%
-      tibble::rownames_to_column(var = "unit") %>%
+    tidy_LL_df = coef_to_pparams(x$fits[z,-c(1,2)])$shared |>
+      tibble::enframe() |>
+      tidyr::pivot_wider() |>
+      dplyr::bind_cols(tidy_LL_df)
+    tidy_ull_df = subset(x$ull, subset = rownames(x$ull) == z) |>
+      t() |>
+      as.data.frame() |>
+      tibble::rownames_to_column(var = "unit") |>
       dplyr::rename(ull = .data$V1)
-    tidy_se_df = subset(x$se, subset = rownames(x$se) == z) %>%
-      t() %>%
-      as.data.frame() %>%
-      tibble::rownames_to_column(var = "unit") %>%
+    tidy_se_df = subset(x$se, subset = rownames(x$se) == z) |>
+      t() |>
+      as.data.frame() |>
+      tibble::rownames_to_column(var = "unit") |>
       dplyr::rename(se = .data$V1)
-    tidy_df = dplyr::left_join(tidy_ull_df, tidy_LL_df, by = "unit") %>%
-      dplyr::left_join(tidy_se_df, by = "unit") %>%
+    tidy_df = dplyr::left_join(tidy_ull_df, tidy_LL_df, by = "unit") |>
+      dplyr::left_join(tidy_se_df, by = "unit") |>
       dplyr::select(
         .data$rep, .data$unit, .data$ull, .data$se, dplyr::everything()
       )
     tidy_df
-  }) %>%
+  }) |>
     dplyr::bind_rows() -> tidy_coef_df
   tidy_coef_df
 }
