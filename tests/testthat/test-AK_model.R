97ccc2ebe146713f9a8476df6e08a4c5734b64bd
Aaron Abkemeier <aaronabkemeier@gmail.com>
2023-06-06 13:33:28 -0400

    Add basic testing stuff

---
+++ tests/testthat/test-AK_model.R
@@ -0,0 +1,11 @@
+test_that("parameters are correct", {
+  expect_equal(AK_mod@shared, AK_pparams$shared)
+  expect_equal(AK_mod@specific, AK_pparams$specific)
+})
+
+test_that("data frames are correct", {
+  answer = readRDS(test_path("key", "AK_dfs.rds"))
+  AK_dfs = lapply(AK_mod@unit.objects, as.data.frame)
+  expect_equal(AK_dfs, answer)
+})
+
