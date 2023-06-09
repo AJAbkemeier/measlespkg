fdb8ce08d3edd773f07a973d3eb75afa6e139268
Aaron Abkemeier <aaronabkemeier@gmail.com>
2023-06-09 14:38:53 -0400

    Clean up evalLogLikOnly.R

--- inst/misc/evalLogLikOnly.R
+++ inst/misc/evalLogLikOnly.R
@@ -23,39 +23,33 @@ LOAD_PARAMS_FROM =
 WRITE_ELL_DIR = "output2/mod_01/group_04/search_01/eval_01/"
 WRITE_FILE = "logLik_eval.rds"
 MAIN_SEED = 7594832
+N_PFILTER_OBJS = 1
 
 set.seed(MAIN_SEED)
 if(!dir.exists(WRITE_ELL_DIR)) dir.create(WRITE_ELL_DIR)
 
 # Get model object
-switch(MODEL_OBJ_FROM,
-  new = {
-    model_obj = AK_model()
-  },
-  fit_results = {
-    fitr = readRDS(LOAD_FIT_FROM)
-    model_obj = fitr[[1]][[1]]
-  }
-)
+if(MODEL_OBJ_FROM == "new"){
+  model_obj = AK_model()
+} else if(MODEL_OBJ_FROM == "fit_results"){
+  fitr = readRDS(LOAD_FIT_FROM)
+  model_obj = fitr[[1]][[1]]
+}
 
 # Get parameters
-switch(PARAMS_FROM,
-  new = {
-    pomp::coef(model_obj) = pomp::coef(AK_model())
-  },
-  fit_results = {
-    fitr = readRDS(LOAD_PARAMS_FROM)
-    fits = fitr[[2]][[1]]$fits
-    best_fit = unlist(fits[order(fits$logLik),][1, -(1:2)])
-    pomp::coef(model_obj) = best_fit
-  },
-  ELL = {
-    fitr = readRDS(LOAD_PARAMS_FROM)
-    fits = fitr$fits
-    best_fit = unlist(fits[order(fits$logLik),][1, -(1:2)])
-    pomp::coef(model_obj) = best_fit
-  }
-)
+if(PARAMS_FROM == "new"){
+  pomp::coef(model_obj) = pomp::coef(AK_model())
+} else if(PARAMS_FROM == "fit_results"){
+  fitr = readRDS(LOAD_PARAMS_FROM)
+  fits = fitr[[2]][[1]]$fits
+  best_fit = unlist(fits[order(fits$logLik),][1, -(1:2)])
+  pomp::coef(model_obj) = best_fit
+} else if(PARAMS_FROM == "ELL"){
+  fitr = readRDS(LOAD_PARAMS_FROM)
+  fits = fitr$fits
+  best_fit = unlist(fits[order(fits$logLik),][1, -(1:2)])
+  pomp::coef(model_obj) = best_fit
+}
 
 ## ----pfilter-----------------------------------------------
 ELL_out = pomp::bake(file = paste0(WRITE_ELL_DIR, WRITE_FILE), {
@@ -66,6 +60,6 @@ ELL_out = pomp::bake(file = paste0(WRITE_ELL_DIR, WRITE_FILE), {
     nreps = NREPS_EVAL,
     seed = NULL,
     divisor = NULL,
-    return_n_pfilter_objs = 1
+    return_n_pfilter_objs = N_PFILTER_OBJS
   )
 })
