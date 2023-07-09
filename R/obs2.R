c030911089328d0a6d2442495f0bc30a300d9932
Aaron Abkemeier <aaronabkemeier@gmail.com>
2023-07-09 17:19:28 -0400

    Add obs2 function

---
+++ R/obs2.R
@@ -0,0 +1,20 @@
+#' Extract the data matrix from a pomp/panelPomp/spatPomp object.
+#'
+#' @param x An object of class `pomp`, `panelPomp`, or `spatPomp`.
+#'
+#' @return A matrix of observations with rows corresponding to units and columns
+#'   corresponding to time points.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' obs2(AK_model())
+#' }
+obs2 = function(x){
+  if(inherits(x, "panelPomp")){
+    out = panelPomp::unitobjects(x) |> sapply(pomp::obs) |> t()
+  } else {
+    out = pomp::obs(x)
+  }
+  out
+}
