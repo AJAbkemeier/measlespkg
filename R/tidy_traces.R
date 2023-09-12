e8782c5186231375f02a324c1ba9e04c6f3bbcb9
Aaron Abkemeier <aaronabkemeier@gmail.com>
2023-09-12 14:57:55 -0400

    Add tidy_traces()

---
+++ R/tidy_traces.R
@@ -0,0 +1,29 @@
+#' Return tidy tibble of traces
+#'
+#' @param x A list of `mif2d.ppomp` objects.
+#'
+#' @return A tibble with columns for replication number, unit name, trace name,
+#'   and trace value. If the parameter in question is shared, the unit name will
+#'   show up as "SHARED".
+#' @export
+#'
+tidy_traces = function(x){
+  stopifnot(is.list(x))
+  lapply(seq_along(x), function(u){
+    panelPomp::traces(x[[u]]) |>
+      as.data.frame() |>
+      tidyr::pivot_longer(dplyr::everything(), names_to = "str") |>
+      dplyr::mutate(rep = u, str = sub("]$", "", .data$str)) |>
+      tidyr::separate_wider_delim(
+        cols = "str",
+        delim = "[",
+        too_few = "align_start",
+        names = c("name", "unit")
+      ) |>
+      dplyr::mutate(
+        unit = ifelse(is.na(.data$unit), "SHARED", .data$unit)
+      )
+  }) |>
+    dplyr::bind_rows() |>
+    dplyr::select("rep", "unit", "name", "value")
+}
