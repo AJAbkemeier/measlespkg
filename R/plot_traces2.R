8891c35a1eec69ecd1d5e7953cb1b60d400e324e
Aaron Abkemeier <aaronabkemeier@gmail.com>
2023-09-22 16:23:10 -0400

    Add new functionality to plot_traces2()

--- R/plot_traces2.R
+++ R/plot_traces2.R
@@ -11,6 +11,10 @@
 #' @param lq Lower quantile cutoff. When shading the traces with the log
 #'   likelihood of the fit, values below the lower quantile are rounded up to
 #'   the lower quantile.
+#' @param pseudo_shared_param Name of a pseudo-shared parameter in the model
+#'   without the number attached at the end. Supplying this argument will cause
+#'   the function to to treat the pseudo-shared parameter as unit-specific. Does
+#'   nothing if set to NULL.
 #'
 #' @return Returns `NULL`.
 #' @export
@@ -20,26 +24,50 @@ plot_traces2 = function(
     plotted_params = ".ALL",
     log_y = FALSE,
     skip_n = 0,
-    lq = 0
+    lq = 0,
+    pseudo_shared_param = NULL
 ){
   stopifnot(inherits(fitr_list[[1]], what = "mif2d.ppomp"))
+
   unit_names = names(fitr_list[[1]]@unit.objects)
-  specific_params = union(rownames(fitr_list[[1]]@specific), "unitLoglik")
-  shared_params = union(names(fitr_list[[1]]@shared), "loglik")
-  traces_tbl = tidy_traces(fitr_list)
+
+  specific_params = union(
+    rownames(fitr_list[[1]]@specific), c("unitLoglik", pseudo_shared_param)
+  )
+  shared_params =
+    setdiff(
+      union(names(fitr_list[[1]]@shared), "loglik"),
+      paste0(pseudo_shared_param, seq_along(unit_names))
+    )
   if(plotted_params != ".ALL"){
     specific_params = intersect(specific_params, plotted_params)
     shared_params = intersect(shared_params, plotted_params)
   }
+
+  traces_tbl = tidy_traces(fitr_list)
+  if(!is.null(pseudo_shared_param)){
+    stopifnot(length(pseudo_shared_param) == 1)
+    psp_regex = paste0("^",pseudo_shared_param,"[1-9][0-9]*$")
+    psp_rows = grepl(psp_regex, traces_tbl$name)
+    psp_tbl = traces_tbl[psp_rows,] |>
+      dplyr::mutate(name = as.numeric(gsub("gamma", "", .data$name))) |>
+      dplyr::mutate(unit = unit_names[.data$name]) |>
+      dplyr::mutate(name = pseudo_shared_param)
+    traces_tbl = traces_tbl |>
+      dplyr::filter(!psp_rows) |>
+      dplyr::bind_rows(psp_tbl)
+  }
+
+  ### Unit-specific traces
   ull_tbl = dplyr::filter(traces_tbl, .data$name == "unitLoglik") |>
     dplyr::select(-"name") |>
     dplyr::rename(ull = .data$value) |>
     dplyr::group_by(.data$unit) |>
     dplyr::mutate(
       ull = ifelse(
-        .data$ull > quantile(.data$ull, lq, na.rm = TRUE),
+        .data$ull > stats::quantile(.data$ull, lq, na.rm = TRUE),
         .data$ull,
-        quantile(.data$ull, lq, na.rm = TRUE)
+        stats::quantile(.data$ull, lq, na.rm = TRUE)
       )
     )
   for(param in specific_params){
@@ -69,15 +97,17 @@ plot_traces2 = function(
       gridExtra::arrangeGrob(grobs = _) |>
       gridExtra::grid.arrange()
   }
+
+  ### Shared traces
   ll_tbl = dplyr::filter(traces_tbl, .data$name == "loglik") |>
     dplyr::select(-"name") |>
     dplyr::rename(ll = .data$value) |>
     dplyr::group_by(.data$unit) |>
     dplyr::mutate(
       ll = ifelse(
-        .data$ll > quantile(.data$ll, lq, na.rm = TRUE),
+        .data$ll > stats::quantile(.data$ll, lq, na.rm = TRUE),
         .data$ll,
-        quantile(.data$ll, lq, na.rm = TRUE)
+        stats::quantile(.data$ll, lq, na.rm = TRUE)
       )
     )
   for(param in shared_params){
